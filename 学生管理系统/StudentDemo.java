d7db063cf1492a9aa2be56f167d574d10c57d8c5
Morry Li <34587749+LING2003@users.noreply.github.com>
2022-12-20 22:34:15 +0800

    目的是为了配合调用

--- 学生管理系统/StudentDemo.java
+++ 学生管理系统/StudentDemo.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class StudentDemo {
-    public static void main(String[] args) {
+    public static void startStudentDemo() {
         ArrayList<Student> list = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         //初始菜单
